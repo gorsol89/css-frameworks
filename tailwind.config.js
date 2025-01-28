3ba3bd7cc495d5e481c2fd9f47706b89ae32f772
gorsol89 <gorsunsol@gmail.com>
2025-01-28 10:09:59 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+	content: ["./**/*.{html,js}","!./node_modules/**/*"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
