cbf74e4e5bc3eee2ec7b1366f4f36df0e46f52c3
gorsol89 <gorsunsol@gmail.com>
2025-02-13 10:35:22 +0100

    issues with deploying on netlify

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  mode: 'jit', // Ensure JIT mode is enabled
+  mode: 'jit', // Ensures Just-In-Time mode is active
   content: [
-    "./**/*.{html,js,ts}", 
+    "./**/*.{html,js}", 
     "!./node_modules/**/*"
   ],
+  theme: {
+    extend: {},
+  },
   safelist: [
     'bg-gray-100', 'text-white', 'text-gray-500', 'text-gray-900',
     'bg-gray-900', 'bg-blue-500', 'hover:bg-blue-600',
     'hover:text-gray-400', 'font-bold', 'text-xl'
-  ], 
-  theme: {
-    extend: {},
-  },
+  ],
   plugins: [],
 };
