41f49172ea14c6f224b0b957e1a4f5f836682a20
gorsol89 <gorsunsol@gmail.com>
2025-02-13 13:06:44 +0100

    test run

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,6 @@
 module.exports = {
-  mode: 'jit', // Just-In-Time mode for fast builds
-  content: [
-    "./src/**/*.{html,js}", // Scans all HTML and JS files inside src
-    "./dist/**/*.{html,js}" // Ensures Tailwind doesn't remove styles
-  ],
-  safelist: [
-    'bg-gray-100', 'text-white', 'text-gray-500', 'text-gray-900',
-    'bg-gray-900', 'bg-blue-500', 'hover:bg-blue-600',
-    'hover:text-gray-400', 'font-bold', 'text-xl'
-  ],
+  mode: 'jit',
+  purge: ['./src/**/*.{html,js}'],
   theme: {
     extend: {},
   },
