f0ce0512e63c2d66e0212fc41bee515bb3a28e9e
gorsol89 <gorsunsol@gmail.com>
2025-02-14 10:06:10 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 // tailwind.config.js
 module.exports = {
-  content: ["./index.html", "./src/**/*.{html,js}"],
+  content: [
+    "./index.html",       // Ensures Tailwind scans your main HTML file
+    "./src/**/*.{html,js}" // Scans all HTML and JS files in src
+  ],
   theme: {
     extend: {}
   },
